6a7915b6ec8229243cabd9edec4af24f2a917b56
ric-vrlab <ric-vrlab@dfki.de>
2019-09-04 18:58:57 +0200

    also forward host porst t this container

--- exec_in_devel.sh
+++ exec_in_devel.sh
@@ -44,6 +44,7 @@ DOCKER_RUN_ARGS=" \
                 --dns $DNSIP \
                 --dns-search=dfki.uni-bremen.de \
                 --expose 7001 --expose 7002 \
+                -p 7001:7001 -p 7002:7002 \
                 -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix \
                 -v $HOST_WORKSPACE/startscripts:/opt/startscripts \
                 -v $HOST_WORKSPACE/workspace/:/opt/workspace -v $HOST_WORKSPACE/home/:/home/devel \
