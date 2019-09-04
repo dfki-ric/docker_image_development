6f9b7e16fc4e8b442dfbe714f238efb2dd4b2fff
ric-vrlab <ric-vrlab@dfki.de>
2019-09-04 18:59:16 +0200

    update simulation to moveit version

--- startscripts/start_simulation.sh
+++ startscripts/start_simulation.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 . /opt/workspace/install/setup.bash
-roslaunch uuv_gazebo start_pid_demo.launch
+#roslaunch uuv_gazebo start_pid_demo.launch
+
+roslaunch mare_it_simulation start_moveit_demo.launch
+
+#roslaunch ros_node_controlled_robot Gazebo_controlled_robot_moveit.launch
+
