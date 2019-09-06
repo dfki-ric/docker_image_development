0fc1b2a895ea349664c48c6ce464a9574f9755c9
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2019-09-06 18:50:50 +0200

    use new controller

--- startscripts/start_simulation.sh
+++ startscripts/start_simulation.sh
@@ -3,7 +3,8 @@
 . /opt/workspace/install/setup.bash
 #roslaunch uuv_gazebo start_pid_demo.launch
 
-roslaunch mare_it_simulation start_moveit_demo.launch
+#roslaunch mare_it_simulation start_moveit_demo.launch
 
 #roslaunch ros_node_controlled_robot Gazebo_controlled_robot_moveit.launch
 
+roslaunch mare_it_control_nodes gazebo_demo_control_simulation.launch
