e60d72f4fb052172ba75a6881569960dade9b4ae
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2020-02-18 11:47:26 +0100

    add some dependencies to main image

--- build_image/build.sh
+++ build_image/build.sh
@@ -1,2 +1,4 @@
 
-docker build -f Dockerfile -t d-reg.hb.dfki.de/mare-it/uuv-sim_18.04:latest .
+docker pull d-reg.hb.dfki.de/rosbasic/melodic_18.04:latest
+
+docker build --no-cache -f Dockerfile -t d-reg.hb.dfki.de/mare-it/uuv-sim_18.04:latest .
