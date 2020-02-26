67597754aaa5716f1b89337cf19b637e4323f7e1
Steffen Planthaber <planthaber@SPLANTHABER-U.dfki.uni-bremen.de>
2020-02-26 16:34:36 +0100

    folder move

--- base_images/ROS_Melodic_18.04/attach_bash.sh
+++
@@ -1,8 +0,0 @@
-
-if [ $# -lt 1  ] || [ $# -gt 1  ] ;then
-     echo "Incorrect usage. Use: docker-run.sh <param1>"
-     echo "<param1>: Give the name of the container you are about to reuse"
-     exit 1
-fi
-
-docker exec -ti $1 /bin/bash
