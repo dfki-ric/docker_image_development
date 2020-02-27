96415b6bd5eae7f260f45b26ef6115870af602ac
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2020-02-27 09:42:22 +0100

    correct names for top level scritps

--- exec_in_devel.bash
+++ exec_in_devel.bash
@@ -12,7 +12,7 @@ xhost +local:root
 #if env already set, use external set value
 INTERACTIVE=${INTERACTIVE:="true"}
 
-. ./docker_commands.sh
+. ./docker_commands.bash
 
 #get a md5 for the current folder used as container name suffix
 #(several checkouts  of this repo possible withtou interfering)
@@ -20,15 +20,19 @@ FOLDER_MD5=$(echo $(pwd) | md5sum | cut -b 1-8)
 
 #use current folder name + devel + path md5 as container name
 #(several checkouts  of this repo possible withtout interfering)
-CONTAINER_NAME=${CONTAINER_NAME:="${PWD##*/}-release-$FOLDER_MD5"}
-IMAGE_NAME="d-reg.hb.dfki.de/mare-it/uuv-sim_18.04:release"
-CONTAINER_ID_FILENAME=release-container_id.txt
+CONTAINER_NAME=${CONTAINER_NAME:="${PWD##*/}-devel-$FOLDER_MD5"}
+IMAGE_NAME="d-reg.hb.dfki.de/mare-it/uuv-sim_18.04:devel"
+CONTAINER_ID_FILENAME=devel-container_id.txt
 
 if [ ! -f $CONTAINER_ID_FILENAME ]; then
     touch $CONTAINER_ID_FILENAME
 fi
 CONTAINER_IMAGE_ID=$(cat $CONTAINER_ID_FILENAME)
 CURRENT_IMAGE_ID=$(docker inspect --format '{{.Id}}' $IMAGE_NAME)
+
+HOST_WORKSPACE=$(pwd)
+mkdir -p $HOST_WORKSPACE/workspace
+mkdir -p $HOST_WORKSPACE/home
 
 DOCKER_RUN_ARGS=" \
                 --name $CONTAINER_NAME \
@@ -38,8 +42,11 @@ DOCKER_RUN_ARGS=" \
                 -u devel \
                 --dns $DNSIP \
                 --dns-search=dfki.uni-bremen.de \
-                -p 7001:7001/tcp -p 7002:7002/tcp \
+                --expose 7001 --expose 7002 \
+                -p 7001:7001 -p 7002:7002 \
                 -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix \
+                -v $HOST_WORKSPACE/startscripts:/opt/startscripts \
+                -v $HOST_WORKSPACE/workspace/:/opt/workspace -v $HOST_WORKSPACE/home/:/home/devel \
                 "
 
 init_docker $@
