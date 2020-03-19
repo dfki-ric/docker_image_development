77c8cc919a140467b5200f5cbb3c1b3ded7867d6
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-03-19 16:13:20 +0100

    added date to files

--- image_setup/04_save_release/build.bash
+++ image_setup/04_save_release/build.bash
@@ -8,20 +8,20 @@ IMAGE_NAME=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_RELEASE_IMAGE
 
 echo "saving ${IMAGE_NAME} to ${PROJECT_NAME}_image_${DATE}.tar.gz"
 
-docker save ${IMAGE_NAME} | gzip > ${PROJECT_NAME}_image.tar.gz
+docker save ${IMAGE_NAME} | gzip > ${PROJECT_NAME}_image_${DATE}.tar.gz
 
 
 #build a scripts zip:
 echo creating scripts archive
 
-mkdir -p ${PROJECT_NAME}_dockerscripts_${DATE}
-cd ${PROJECT_NAME}_dockerscripts_${DATE}
+mkdir -p ${PROJECT_NAME}_scripts_${DATE}
+cd ${PROJECT_NAME}_scripts_${DATE}
 cp ../../../docker_commands.bash ./
 cp ../../../settings.bash ./
 cp ../../../exec_in_release.bash ./
 cp ../../../stop_release_container.bash ./
 cp ../Readme_scripts.md ./Readme.md
 cd ..
-tar czf ${PROJECT_NAME}_dockerscripts.tar.gz ${PROJECT_NAME}_dockerscripts_${DATE}.tar.gz
+tar czf ${PROJECT_NAME}_scripts_${DATE}.tar.gz ${PROJECT_NAME}_scripts_${DATE}
 
-rm -rf ${PROJECT_NAME}_dockerscripts
+rm -rf ${PROJECT_NAME}_scripts_${DATE}
