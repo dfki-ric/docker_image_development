410ae8e765b62034673bb02d97903cc5c8410328
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-03-23 08:23:52 +0100

    add rosdep install

--- image_setup/01_base_images/install_ros_dependencies.sh
+++ image_setup/01_base_images/install_ros_dependencies.sh
@@ -13,7 +13,16 @@ apt-get update && apt-get install -y \
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-apt-get update && apt-get install -y ros-melodic-desktop python-rosinstall python-rosinstall-generator python-wstool python-catkin-tools
+apt-get update && apt-get install -y \
+    ros-melodic-desktop \
+    ros-melodic-viz \
+    python-rosinstall \
+    python-rosinstall-generator \
+    python-wstool \
+    python-rosdep \
+    python-catkin-tools
+
+
 rosdep init && rosdep update
 
 
