3ca4d9b6d5528adfbc3f6971f5d17aa19f00b6f2
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-03-24 15:52:38 +0100

    add autocomplete to 04 scripts

--- image_setup/04_save_release/build.bash
+++ image_setup/04_save_release/build.bash
@@ -20,8 +20,12 @@ cp ../../../docker_commands.bash ./
 cp ../../../settings.bash ./
 cp ../../../exec_in_release.bash ./
 cp ../../../stop_release_container.bash ./
+echo "complete -W "$(for scripts in $(ls ../../startscripts); do printf $scripts' '; done)/bin/bash" ./exec_in_release.bash" > autocomplete.me
 cp ../Readme_scripts.md ./Readme.md
+
 cd ..
 tar czf ${PROJECT_NAME}_scripts_${DATE}.tar.gz ${PROJECT_NAME}_scripts_${DATE}
 
 rm -rf ${PROJECT_NAME}_scripts_${DATE}
+
+
