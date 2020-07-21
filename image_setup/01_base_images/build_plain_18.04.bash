a6a9a811470ebf2fa5314a2aacc1f1e25c02a665
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-07-21 16:34:04 +0200

    tag base images as :base instead :latest

--- image_setup/01_base_images/build_plain_18.04.bash
+++ image_setup/01_base_images/build_plain_18.04.bash
@@ -6,11 +6,11 @@ export BASE_IMAGE=nvidia/opengl:1.0-glvnd-devel-ubuntu18.04
 export INSTALL_SCRIPT=install_plain_dependencies.bash
 
 docker pull $BASE_IMAGE
-docker build --no-cache -f Dockerfile -t $IMAGE_NAME:latest --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT .
+docker build --no-cache -f Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT .
 
 
 
 echo
 echo "don't forget to push the image if you wish:"
-echo "docker push $IMAGE_NAME:latest"
+echo "docker push $IMAGE_NAME:base"
 echo
--- settings.bash
+++ settings.bash
@@ -14,9 +14,9 @@ export DEFAULT_EXECMODE="base" # Use this only for setting up the initial devel 
 
 # The base image used when building a workspace image (one of the ones build in base_images)
 
-# export WORKSPACE_BASE_IMAGE=docker_development/rock_master_18.04:latest # image with rock core dependencies installed
-# export WORKSPACE_BASE_IMAGE=docker_development/ros_melodic_18.04:latest # image with basic ros installed
-export WORKSPACE_BASE_IMAGE=docker_development/plain_18.04:latest # plain image with build_essentials installed
+# export WORKSPACE_BASE_IMAGE=docker_development/rock_master_18.04:base # image with rock core dependencies installed
+# export WORKSPACE_BASE_IMAGE=docker_development/ros_melodic_18.04:base # image with basic ros installed
+export WORKSPACE_BASE_IMAGE=docker_development/plain_18.04:base # plain image with build_essentials installed
 
 
 # The Name of the Workspace image to use
