c9155bc7399ac9586f467a71c96b91dc865fedfd
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-07-22 11:59:41 +0200

    image settings for update script

--- tools/update_workspace_images.bash
+++ tools/update_workspace_images.bash
@@ -2,9 +2,33 @@
 
 . ../settings.bash
 
-DEVEL_IMAGE=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_DEVEL_IMAGE
-RELEASE_IMAGE=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_RELEASE_IMAGE
+
+IMAGE=""
+EXECMODE=$DEFAULT_EXECMODE
+
+if [ "$1" = "base" ]; then
+    EXECMODE="base"
+    shift
+fi
+if [ "$1" = "devel" ]; then
+    EXECMODE="devel"
+    shift
+fi
+if [ "$1" = "release" ]; then
+    EXECMODE="release"
+    shift
+fi
+if [ "$EXECMODE" = "base" ]; then
+    IMAGE=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_BASE_IMAGE
+fi
+if [ "$EXECMODE" = "devel" ]; then
+    IMAGE=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_DEVEL_IMAGE
+fi
+if [ "$EXECMODE" = "release" ]; then
+    IMAGE=${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}$WORKSPACE_RELEASE_IMAGE
+fi
 
 
-docker pull $DEVEL_IMAGE
-docker pull $RELEASE_IMAGE
+
+echo "pulling image ${IMAGE}"
+#docker pull $IMAGE
