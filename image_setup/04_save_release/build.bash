f59d1ec3df6aaf5251b250919a15faf4ac491e85
Steffen Planthaber <steffen.planthaber@dfki.de>
2020-11-11 17:43:34 +0100

    Update build.bash for save_release
    * Fix setup_docker filename
    * collect scripts first

--- image_setup/04_save_release/build.bash
+++ image_setup/04_save_release/build.bash
@@ -15,27 +15,20 @@ IMAGE_NAME=${RELEASE_REGISTRY:+${RELEASE_REGISTRY}/}$WORKSPACE_RELEASE_IMAGE
 SCRIPTFOLDER=${PROJECT_NAME_NO_SUBFOLDER}_scripts_${DATE}
 
 mkdir -p $SCRIPTFOLDER
-
-
-echo "saving ${IMAGE_NAME} to ${PROJECT_NAME_NO_SUBFOLDER}_image_${DATE}.tar.gz"
-
-docker save ${IMAGE_NAME} | gzip > ${PROJECT_NAME_NO_SUBFOLDER}_image_${DATE}.tar.gz
-
-
 #build a scripts zip:
-echo creating scripts archive
+echo "creating scripts archive: $SCRIPTFOLDER.tar.gz"
 cp ../../docker_commands.bash ./$SCRIPTFOLDER/
 cp ../../settings.bash ./$SCRIPTFOLDER/
 cp ../../exec.bash ./$SCRIPTFOLDER/
 cp ../../stop.bash ./$SCRIPTFOLDER/
-cp ../../doc/00_Setup_Docker.md ./$SCRIPTFOLDER/Readme_Docker.md
-
-echo "complete -W \"$(ls ../../startscripts | xargs) /bin/bash\" ./exec.bash" >> $SCRIPTFOLDER/autocomplete.me
-
+cp ../../doc/010_Setup_Docker.md ./$SCRIPTFOLDER/Readme_Docker.md
 cp ./Readme_scripts.md ./$SCRIPTFOLDER/Readme.md
-
+echo "complete -W \"$(ls ../../startscripts | xargs) /bin/bash\" ./exec.bash" >> $SCRIPTFOLDER/autocomplete.me
 tar czf $SCRIPTFOLDER.tar.gz $SCRIPTFOLDER
-
 rm -rf $SCRIPTFOLDER
+
+
+echo "saving ${IMAGE_NAME} to ${PROJECT_NAME_NO_SUBFOLDER}_image_${DATE}.tar.gz"
+docker save ${IMAGE_NAME} | gzip > ${PROJECT_NAME_NO_SUBFOLDER}_image_${DATE}.tar.gz
 
 
