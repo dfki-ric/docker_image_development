ba766600b1b461fd59555285c1c413b4e71b1816
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2020-11-16 17:57:18 +0100

    Add ros-noetic and ros-noetic base to base images

--- image_setup/01_base_images/install_ros_minimal_dependencies.bash
+++ image_setup/01_base_images/install_ros_minimal_dependencies.bash
@@ -3,23 +3,26 @@
 #install ROS melodic (second install because needs gnupg)
 export DEBIAN_FRONTEND=noninteractive
 
+DISTRO=${1:-melodic}
+[ $DISTRO == "noetic" ] && export vPYTHON=3
+
 apt-get update && apt-get install -y \
     lsb-release \
     gnupg2 \
     gosu \
     ruby \
     ruby-dev \
-    python
+    python$vPYTHON
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt-get update && apt-get install -y \
-    ros-melodic-ros-base \
-    python-rosinstall \
-    python-rosinstall-generator \
-    python-wstool \
-    python-rosdep \
-    python-catkin-tools
+    ros-$DISTRO-ros-base \
+    python$vPYTHON-rosinstall \
+    python$vPYTHON-rosinstall-generator \
+    python$vPYTHON-wstool \
+    python$vPYTHON-rosdep \
+    python$vPYTHON-catkin-tools
 
 
 #rosdep init && rosdep update
