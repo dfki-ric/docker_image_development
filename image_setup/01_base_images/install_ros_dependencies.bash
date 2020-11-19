db3902eb0f0543ee774a0c77a73463629b4e25f6
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2020-11-19 14:01:49 +0100

    Add python*-osrf-pycommon to base image for catkin to work with noetic distro

--- image_setup/01_base_images/install_ros_dependencies.bash
+++ image_setup/01_base_images/install_ros_dependencies.bash
@@ -18,11 +18,12 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt-get update && apt-get install -y \
     ros-$DISTRO-desktop \
+    python$vPYTHON-catkin-tools \
+    python$vPYTHON-osrf-pycommon \
+    python$vPYTHON-rosdep \
     python$vPYTHON-rosinstall \
     python$vPYTHON-rosinstall-generator \
-    python$vPYTHON-wstool \
-    python$vPYTHON-rosdep \
-    python$vPYTHON-catkin-tools
+    python$vPYTHON-wstool
 
 
 #rosdep init && rosdep update
