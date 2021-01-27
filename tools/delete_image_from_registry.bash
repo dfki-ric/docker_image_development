e119121578b870235917a4752e43dc43b4d850a9
Steffen Planthaber <steffen.planthaber@dfki.de>
2021-01-27 15:10:41 +0100

    add tool to delete images from docker registry

---
+++ tools/delete_image_from_registry.bash
@@ -0,0 +1,27 @@
+# helper tool to delete tags from a docker registy
+# Need to run garbage collection afterwards in oder to free disk space https://docs.docker.com/registry/garbage-collection/
+# $> docker exec registry bin/registry garbage-collect --dry-run /etc/docker/registry/config.yml
+
+#https://stackoverflow.com/questions/25436742/how-to-delete-images-from-a-private-docker-registry
+if [$# -ne 4]; then
+ echo "illegal number of parameters"
+ echo use: RegistryUsername registryURL imagename tag
+fi
+
+USER=$1
+REGISTRY=$2
+NAME=$3
+TAG=$4
+read -sp 'registry password:' PASSWD 
+
+echo
+
+# get digest value:
+DIGEST=$(curl -v --silent -u ${USER}:${PASSWD} -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -X GET https://${REGISTRY}/v2/${NAME}/manifests/${TAG} 2>&1 | grep docker-content-digest | awk '{print ($3)}')
+
+set -x
+echo deleting $DIGEST
+
+#actually delete
+curl -v -u ${USER}:${PASSWD} -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -X DELETE https://${REGISTRY}/v2/${NAME}/manifests/${DIGEST/$'\r'/}
+
