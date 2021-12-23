9b2c93814bf58fde2b11efb5368695de5df4f56e
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2021-12-23 18:46:35 +0100

    Tag CD image with old release registry

--- image_setup/continuous_deployment/docker_push.bash
+++ image_setup/continuous_deployment/docker_push.bash
@@ -2,7 +2,6 @@
 
 ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )
 source $ROOT_DIR/settings.bash
-source $ROOT_DIR/docker_commands.bash #Included for registry override
 
 CD_IMAGE_NAME=${RELEASE_REGISTRY:+${RELEASE_REGISTRY}/}$WORKSPACE_CD_IMAGE
 docker push $CD_IMAGE_NAME
