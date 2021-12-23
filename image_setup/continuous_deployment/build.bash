f7783ad42248d76eea43b3cb6160eff6af556dd1
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2021-12-23 14:35:24 +0100

    fix test script execution

--- image_setup/continuous_deployment/build.bash
+++ image_setup/continuous_deployment/build.bash
@@ -94,7 +94,7 @@ build_devel_image
 bash ${CD_ROOT_DIR}/image_setup/03_release_image/build.bash
 
 # run the tests
-./exec.bash release /opt/startscripts/continuous_deployment_hooks/test
+./exec.bash release "/opt/startscripts/continuous_deployment_hooks/test"
 
 RELEASE_IMAGE_NAME=${RELEASE_REGISTRY:+${RELEASE_REGISTRY}/}$WORKSPACE_RELEASE_IMAGE
 CD_IMAGE_NAME=${RELEASE_REGISTRY:+${RELEASE_REGISTRY}/}$WORKSPACE_CD_IMAGE
