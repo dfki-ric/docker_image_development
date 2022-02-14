7237c0416950ecba065f6fffaabdc462260b2aa4
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2022-02-14 12:46:47 +0100

    Add tools script to delete dangling images

---
+++ tools/delete_dangling_images.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# list all images that are dangling
+(echo -e "\n\e[1m\e[33mWARNING: The following images would be deleted:\n\n\e[0m" && docker images -f dangling=true ) | less -r -X
+
+
+# prompt for deletion
+read -p "    => Do you want to delete all listed images? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    echo
+    for hash in $(docker images -f dangling=true -q); do docker rmi -f $hash; done
+    echo
+else
+    echo 
+    echo "As you wish, I won't do anything."
+    exit 1
+fi
