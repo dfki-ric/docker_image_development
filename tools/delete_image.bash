6c94982285ef3d44609b242092c3a880d7b981e4
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2022-04-19 16:51:29 +0200

    Fix tools/update_image script

--- tools/delete_image.bash
+++ tools/delete_image.bash
@@ -2,6 +2,7 @@
 
 ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 source $ROOT_DIR/settings.bash
+source $ROOT_DIR/.docker_scripts/variables.bash
 
 evaluate_execmode $1
 set_image_name $EXECMODE
