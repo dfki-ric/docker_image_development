be3d3dc940af62f7fd68a24c9a52fa6310918e3e
mlodhi <haider_khan.lodhi@dfki.de>
2022-06-21 13:56:39 +0200

    Add ros2 foxy base image setup

---
+++ image_setup/01_base_images/build_ros2_foxy_20.04.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+#copy scripts VERSION file to put it into the image
+cp ../../VERSION ./
+
+. ../../settings.bash
+export IMAGE_NAME=${BASE_REGISTRY:+${BASE_REGISTRY}/}developmentimage/ros2_foxy_20.04
+
+export BASE_IMAGE=nvidia/opengl:1.2-glvnd-devel-ubuntu20.04
+export INSTALL_SCRIPT=install_ros2_dependencies.bash
+export INSTALL_ARGS=foxy
+
+docker pull $BASE_IMAGE
+docker build --no-cache -f Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --build-arg INSTALL_ARGS --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" .
+
+# remove VERSION file from here
+rm -rf VERSION
+
+echo
+echo "don't forget to push the image if you wish:"
+echo "docker push $IMAGE_NAME:base"
+echo
+
