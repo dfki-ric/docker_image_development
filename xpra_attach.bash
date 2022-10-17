1a8fe9df371a0741a7f01e1645a006620adaa75c
Leon Cedric Danter <leon_cedric.danter@dfki.de>
2022-10-17 17:39:38 +0200

    Check if xpra is installed in xpra_attach.bash

--- xpra_attach.bash
+++ xpra_attach.bash
@@ -3,4 +3,8 @@
 ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $ROOT_DIR/.docker_scripts/variables.bash
 
+command -v xpra > /dev/null 2>&1
+if [ "$?" -ne "0" ]; then
+    echo -e "Please install xpra:\n    sudo apt install xpra" && exit 13
+fi
 xpra attach --sharing=yes tcp:127.0.0.1:$XPRA_PORT
