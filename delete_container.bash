3ec5e0acd177252c2ff286ee08739d2c1219250f
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2022-10-24 18:10:05 +0200

    Update delete_container.bash
    the container_name can be overridden in the settings.bash, respect that in the delete_container script

--- delete_container.bash
+++ delete_container.bash
@@ -3,13 +3,14 @@
 ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $ROOT_DIR/.docker_scripts/file_handling.bash
 source $ROOT_DIR/.docker_scripts/variables.bash
+source $ROOT_DIR/settings.bash
 
 # stop on error
 set -e
 
 evaluate_execmode $1
 
-CONTAINER_NAME="${ROOT_DIR##*/}-$EXECMODE-$FOLDER_MD5"
+CONTAINER_NAME=${CONTAINER_NAME:="${ROOT_DIR##*/}-$EXECMODE-$FOLDER_MD5"}
 
 $PRINT_INFO "stopping ${CONTAINER_NAME}"
 docker stop ${CONTAINER_NAME} > /dev/null || true
