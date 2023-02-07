3330c3bca164964be298682701e0a11ba8f2e5d3
haider8645 <haider_khan.lodhi@dfki.de>
2023-02-07 15:17:48 +0100

    Add base image for ros2 humble on 22.04 (#67)
    * Add base image for ros2 humble on 22.04
    
    * Change base image to ubuntu:22.04 and add _nogl to image name

--- image_setup/01_base_images/install_ros2_dependencies.bash
+++ image_setup/01_base_images/install_ros2_dependencies.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#install ROS2 Foxy 
+#install ROS2
 export DEBIAN_FRONTEND=noninteractive
 export vPYTHON=3
-export DISTRO="foxy"
+export DISTRO=${1:-foxy}
 
 apt update && apt install -y \
     lsb-release \
@@ -13,16 +13,15 @@ apt update && apt install -y \
     locales \
     software-properties-common
 
-
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-sudo add-apt-repository universe
+add-apt-repository universe
 
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 apt update && apt upgrade -y &&  apt install -y \
     ros-$DISTRO-desktop \
