33900e0e45ff27a94c752772fbbf820ee6392b9a
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-05-03 17:09:08 +0200

    add working docker-compose file for minimal release

--- image_setup/03_release_minimal/entrypoint.bash
+++ image_setup/03_release_minimal/entrypoint.bash
@@ -3,7 +3,7 @@
 echo $1
 
 if [ "$1" == "run" ]; then
-    /cloud_slam_server
+    /cloud_slam_server --port $LISTEN_PORT --redis_ip $REDIS_IP --redis_port $REDIS_PORT --neo4j_ip $NEO4J_IP --neo4j_port $NEO4J_PORT
     # exit with error if app fails
     exit 1
 fi
