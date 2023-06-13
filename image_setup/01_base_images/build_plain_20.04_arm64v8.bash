4f6d84fc61cd96e5995f191bc2f8b6237aa58a85
Anna Born <anna.born@dfki.de>
2023-06-13 16:11:15 +0200

    add base image build script for ubuntu 20.04 and 22.04 arm64 (#73)

---
+++ image_setup/01_base_images/build_plain_20.04_arm64v8.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+#copy scripts VERSION file to put it into the image
+cp ../../VERSION ./
+
+. ../../settings.bash
+export IMAGE_NAME=${BASE_REGISTRY:+${BASE_REGISTRY}/}developmentimage/plain_20.04_arm64v8
+
+export BASE_IMAGE=arm64v8/ubuntu:20.04
+export INSTALL_SCRIPT=install_plain_dependencies.bash
+
+docker pull $BASE_IMAGE
+docker build --no-cache -f Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" .
+
+# remove VERSION file from here
+rm -rf VERSION
+
+echo
+echo "don't forget to push the image if you wish:"
+echo "docker push $IMAGE_NAME:base"
+echo
