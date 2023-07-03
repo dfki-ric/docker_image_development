efd13c52bb94269269ef13a894a3da456f9babf9
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-07-03 14:45:23 +0200

    use xterm to set xhost +local:root if launch is from ssh remote

--- exec.bash
+++ exec.bash
@@ -6,14 +6,26 @@ source $ROOT_DIR/.docker_scripts/exec.bash
 
 #allow local connections of root (docker daemon) to the current users x server
 if [ "$DOCKER_XSERVER_TYPE" = "mount" ] && command -v xhost > /dev/null; then
-    xhost +local:root > /dev/null
+    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+        # this is an ssh shell
+        xterm -e "xhost +local:root"
+    else
+        xhost +local:root > /dev/null
+    fi
 fi
+
+
 
 init_docker $@
 
 # remove permission for local connections of root (docker daemon) to the current users x server
 if [ "$DOCKER_XSERVER_TYPE" = "mount" ] && command -v xhost > /dev/null; then
-    xhost -local:root > /dev/null
+    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+        # this is an ssh shell
+        xterm -e "xhost -local:root"
+    else
+        xhost -local:root > /dev/null
+    fi
 fi
 
 # set by $ROOT_DIR/.docker_scripts/exec.bash
