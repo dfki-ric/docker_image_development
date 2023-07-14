295a8adc928e6f3b386b1baf7e34681263645d6a
Steffen Planthaber <steffen.planthaber@dfki.de>
2023-07-14 12:11:08 +0200

    auto-add --privileged where needed with warning

--- .docker_scripts/docker_commands.bash
+++ .docker_scripts/docker_commands.bash
@@ -4,6 +4,8 @@ ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 source $ROOT_DIR/settings.bash
 source $ROOT_DIR/.docker_scripts/variables.bash
 source $ROOT_DIR/.docker_scripts/file_handling.bash
+source $ROOT_DIR/.docker_scripts/helper_functions.bash
+
 
 #storage variable for the return value of the docker exec command
 DOCKER_EXEC_RETURN_VALUE=1
@@ -55,6 +57,10 @@ init_docker(){
         RUNTIME_ARG="--gpus all"
     else
         $PRINT_WARNING "hardware acceleration disabled"
+        if [[ "$DOCKER_RUN_ARGS" != *" --privileged "* ]]; then
+            $PRINT_WARNING "addind: '--privileged' to run args to allow usage of X apps, add it to the ADDITIONAL_DOCKER_RUN_ARGS in your settings.bash file to supress this warning"
+            DOCKER_RUN_ARGS=$(add_param_if_not_present "${DOCKER_RUN_ARGS}" --privileged)
+        fi
     fi
 
     if "$INTERACTIVE"; then
@@ -66,9 +72,13 @@ init_docker(){
     if [ $NEEDS_DOCKER_IN_CONTAINER = true ]; then
         DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3)
         $PRINT_DEBUG "Setting up docker usage inside the container"
-        $PRINT_DEBUG "adding: '--privileged -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_GROUP_ID=$DOCKER_GROUP_ID' to run args"
+        if [[ "$DOCKER_RUN_ARGS" != *" --privileged "* ]]; then
+            $PRINT_WARNING "adding: '--privileged' to run args to allow using docker from the container, add it to the ADDITIONAL_DOCKER_RUN_ARGS in your settings.bash file to supress this warning"
+            DOCKER_RUN_ARGS=$(add_param_if_not_present "${DOCKER_RUN_ARGS}" --privileged)
+        fi
+        $PRINT_DEBUG "adding: '-v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_GROUP_ID=$DOCKER_GROUP_ID' to run args"
         $PRINT_DEBUG "Setting docker group id to $DOCKER_GROUP_ID inside the container"
-        DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS --privileged -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_GROUP_ID=$DOCKER_GROUP_ID"
+        DOCKER_RUN_ARGS="$DOCKER_RUN_ARGS -v /var/run/docker.sock:/var/run/docker.sock -e DOCKER_GROUP_ID=$DOCKER_GROUP_ID"
     fi
 
     # check if a container from previous runs exist
