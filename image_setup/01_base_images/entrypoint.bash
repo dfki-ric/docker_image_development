55c5f94fdd18ac01a8364d765aa87226950a34c2
Steffen Planthaber <steffen.planthaber@dfki.de>
2024-05-13 14:21:24 +0200

    only run uid change on initial run with init_container cmd

--- image_setup/01_base_images/entrypoint.bash
+++ image_setup/01_base_images/entrypoint.bash
@@ -76,46 +76,45 @@ else
     fi
 fi
 
-
-# initialize the base and devel container, set the correct UID from host
-if [ ! -f /initialized_container ]; then
-    $PRINT_INFO
-    $PRINT_INFO -e "\e[33mSetting the containers devel user id to host user id\e[0m"
-    $PRINT_INFO
-    #only executed by docker run
-    sudo touch /initialized_container
-    
-    # create ccache dir, if variable set (enabled in settings and CCACHE_DIR set in run command)
-    if [ ! -z $CCACHE_DIR ]; then
-        sudo mkdir -p $CCACHE_DIR
-        # the dockeruser might still have the wrong id, so using the NUID here
-        sudo chown $NUID $CCACHE_DIR
-    else
-        unset CCACHE_DIR
-    fi
-
-    # use -E to keep env (for PRINT_* environment)
-    sudo -E /bin/bash /opt/init_user_id.bash
-    # id script needs exit to apply uid next docker start, so exiting here
-    # the exec script expects this to happen and rund start/exec afterwards
-fi
-
 # The init_user_id.bash change of a uid requires the container to exit, but we don't want that bahavior when running custom commands
 # so the CMD in the dockerfile is "init_container" to detect a default start, translated to /bin/bash when no exit is required (already exited once)
 # in case docker run is called with a command (or startscript), that one is executed directly
+
+# init_container is the default cmd in the Dockerfile, executed on docker run AND docker start
+# this part is executed when the default command is used
 if [ "$@" == "init_container" ]; then
-    # init_container is the default cmd, executed on docker run AND docker start
-    # this part is executed when the default command is used
-    if [ ! -f /start_already_exited ]; then
-        # on first start (rocker run) on default arg (used by exec stript), we need to exit to update a potentially changed uid 
-        sudo touch /start_already_exited
+    if [ ! -f /initialized_container ]; then
+        # only executed by initial docker run, not on docker start
+        # releases are creating this file in thrir Dockerfile, so this is not run at all from release images
+        sudo touch /initialized_container
+        
+        $PRINT_INFO
+        $PRINT_INFO -e "\e[33mSetting the containers devel user id to host user id\e[0m"
+        $PRINT_INFO
+
+        # create ccache dir, if variable set (enabled in settings and CCACHE_DIR set in run command)
+        if [ ! -z $CCACHE_DIR ]; then
+            sudo mkdir -p $CCACHE_DIR
+            # the dockeruser might still have the wrong id, so using the NUID here
+            sudo chown $NUID $CCACHE_DIR
+        else
+            unset CCACHE_DIR
+        fi
+
+        # initialize the container, set the correct UID from host
+        # use -E to keep env (for PRINT_* environment)
+        sudo -E /bin/bash /opt/init_user_id.bash
+        # id script needs exit to apply uid next docker start, so exiting here
+        # the exec script expects this to happen and rund start/exec afterwards
         exit 0
-    else
-        # subsequent docker start should start a console as default 
+    else 
+        # subsequent docker start commands should start a console as default (these scripts are using exec to launch additional stuff)
+        # this keeps the container running
         exec "/bin/bash"
     fi
 else
-    # if a non-default cmd is set after docker run, use that one
+    # if a non-default cmd is set after docker run, use that one, allows to directly run contiansers via e.g. docker-compose
+    # in case a custom command is given (even /bin/bash)
     exec "$@"
 fi
 
