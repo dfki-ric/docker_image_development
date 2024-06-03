92964e7478d7f26c4d1e7004071605fa6c804516
0Nel <leon_cedric.danter@dfki.de>
2024-06-03 09:23:27 +0200

    Patch entrypoint (#93)
    * Add confusing line in CD build script
    
    * patch entrypoint for docker compose & build base img from anywhere

--- image_setup/01_base_images/build_rock_master_20.04.bash
+++ image_setup/01_base_images/build_rock_master_20.04.bash
@@ -2,19 +2,21 @@
 set -e
 
 #copy scripts VERSION file to put it into the image
-cp ../../VERSION ./
+THIS_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )
+cp $ROOT_DIR/VERSION $THIS_DIR/
 
-. ../../settings.bash
+. $ROOT_DIR/settings.bash
 export IMAGE_NAME=${BASE_REGISTRY:+${BASE_REGISTRY}/}developmentimage/rock_master_20.04
 
 export BASE_IMAGE=nvidia/opengl:1.2-glvnd-devel-ubuntu20.04
 export INSTALL_SCRIPT=install_rock_20.04_dependencies.bash
 
 docker pull $BASE_IMAGE
-docker build --no-cache -f Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" .
+docker build --no-cache -f $THIS_DIR/Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" $THIS_DIR
 
 # remove VERSION file from here
-rm -rf VERSION
+rm -rf $THIS_DIR/VERSION
 
 echo
 echo "don't forget to push the image if you wish:"
