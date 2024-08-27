339f05d968828258388dc825539abfad64680e0b
Leon Danter <leon_cedric.danter@dfki.de>
2024-08-27 17:39:52 +0200

    Fix THIS_DIR path

--- image_setup/01_base_images/build_ros2_humble_22.04_arm64v8_nogl.bash
+++ image_setup/01_base_images/build_ros2_humble_22.04_arm64v8_nogl.bash
@@ -11,12 +11,13 @@ export IMAGE_NAME=${BASE_REGISTRY:+${BASE_REGISTRY}/}developmentimage/ros2_humbl
 
 export BASE_IMAGE=arm64v8/ubuntu:22.04
 export INSTALL_SCRIPT=install_ros2_dependencies.bash
+export INSTALL_ARGS=humble
 
 docker pull $BASE_IMAGE
-docker build --no-cache -f Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" .
+docker build --no-cache -f $THIS_DIR/Dockerfile -t $IMAGE_NAME:base --build-arg BASE_IMAGE --build-arg INSTALL_SCRIPT --build-arg INSTALL_ARGS --label "base-image-name=$IMAGE_NAME:base" --label "base-image-created-from=${BASE_IMAGE} - $(docker inspect --format '{{.Id}}' $BASE_IMAGE)" --label "dockerfile_repo_commit=$(git rev-parse HEAD)" $THIS_DIR
 
 # remove VERSION file from here
-rm -rf VERSION
+rm -rf $THIS_DIR/VERSION
 
 echo
 echo "don't forget to push the image if you wish:"
