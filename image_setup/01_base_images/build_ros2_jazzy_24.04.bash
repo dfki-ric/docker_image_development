fba59181d68ff7535fb433f59eb246228101fc9d
Steffen Planthaber <steffen.planthaber@dfki.de>
2025-02-14 11:53:13 +0100

    fix os tag for ros2_jazzy

--- image_setup/01_base_images/build_ros2_jazzy_24.04.bash
+++ image_setup/01_base_images/build_ros2_jazzy_24.04.bash
@@ -2,4 +2,4 @@
 THIS_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $THIS_DIR/build_base_image.bash
 
-build_base_image ubuntu:24.04 ros2_jazzy_22.04 install_ros2_dependencies.bash
+build_base_image ubuntu:24.04 ros2_jazzy_24.04 install_ros2_dependencies.bash
