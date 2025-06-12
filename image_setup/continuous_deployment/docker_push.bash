d00d7bdfc4468881489864dec9d74d83b3f0f057
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2025-06-12 13:21:32 +0200

    fix: CD builds now use frozen (#111)

--- image_setup/continuous_deployment/docker_push.bash
+++ image_setup/continuous_deployment/docker_push.bash
@@ -5,5 +5,5 @@ source $ROOT_DIR/settings.bash
 source $ROOT_DIR/.docker_scripts/variables.bash
 
 check_registry_overrides
-CD_IMAGE_NAME=${RELEASE_REGISTRY:+${RELEASE_REGISTRY}/}$WORKSPACE_CD_IMAGE
+CD_IMAGE_NAME=${FROZEN_REGISTRY:+${FROZEN_REGISTRY}/}$WORKSPACE_CD_IMAGE
 docker push $CD_IMAGE_NAME
